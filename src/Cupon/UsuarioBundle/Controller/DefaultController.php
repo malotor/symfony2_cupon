3ce1dbee868fd7b0d7181eb062776b04eebe0847
Manel <malotor@gmail.com>
2014-08-26 23:51:20 +0200

    Add login form

--- src/Cupon/UsuarioBundle/Controller/DefaultController.php
+++ src/Cupon/UsuarioBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace Cupon\UsuarioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\Security\Core\SecurityContext;
+
 class DefaultController extends Controller
 {
   public function comprasAction()
@@ -19,5 +21,32 @@ class DefaultController extends Controller
     return $this->render('UsuarioBundle:Default:compras.html.twig', [
       'compras'  => $compras
     ]);
+  }
+
+  public function loginAction()
+  {
+    $peticion = $this->getRequest();
+    $sesion = $peticion->getSession();
+    $error = $peticion->attributes->get(
+      SecurityContext::AUTHENTICATION_ERROR,
+      $sesion->get(SecurityContext::AUTHENTICATION_ERROR)
+    );
+    return $this->render('UsuarioBundle:Default:login.html.twig', array(
+      'last_username' => $sesion->get(SecurityContext::LAST_USERNAME),
+      'error'         => $error
+    ));
+  }
+
+  public function cajaLoginAction() {
+    $peticion = $this->getRequest();
+    $sesion = $peticion->getSession();
+    $error = $peticion->attributes->get(
+      SecurityContext::AUTHENTICATION_ERROR,
+      $sesion->get(SecurityContext::AUTHENTICATION_ERROR)
+    );
+    return $this->render('UsuarioBundle:Default:login.html.twig', array(
+      'last_username' => $sesion->get(SecurityContext::LAST_USERNAME),
+      'error'         => $error
+    ));
   }
 }
